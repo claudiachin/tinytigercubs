44ebaa851260c296ed0820b6540b8f1020b64606
Claudia Chin <50474688+claudiachin@users.noreply.github.com>
2022-03-15 01:51:18 +0800

    faq done

--- src/index.js
+++ src/index.js
@@ -9,3 +9,15 @@ function onMenuClick() {
         menuicon.src = "src/imgs/menu-icon.png";
     }
 }
+
+function toggleAns(toggle) {
+    if (toggle.innerHTML == '+') { // to open
+        toggle.innerHTML = '-';
+        toggle.parentElement.style.borderRadius = '30px 30px 0 0'
+        toggle.parentElement.nextElementSibling.style.display = 'block';
+    } else {
+        toggle.innerHTML = '+';
+        toggle.parentElement.style.borderRadius = '30px'
+        toggle.parentElement.nextElementSibling.style.display = 'none';
+    }
+}
