2d98ba8877f96432737268e99e77dde7d502eacc
Claudia Chin <50474688+claudiachin@users.noreply.github.com>
2022-03-14 21:30:21 +0800

    fix more links

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ function onMenuClick() {
     let menu = document.getElementById("menu");
     if (menu.style.display == 'none') {
         menu.style.display = 'flex';
-        menuicon.src = "src/close-icon.png";
+        menuicon.src = "src/imgs/close-icon.png";
     } else {
         menu.style.display = 'none';
-        menuicon.src = "src/menu-icon.png";
+        menuicon.src = "src/imgs/menu-icon.png";
     }
 }
